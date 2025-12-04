a7312e73b04b2fa055bc807b86890c78beea92f7
Anton Redkozubov <uacreez@gmail.com>
2025-12-04 12:22:42 +0300

    Initialize project and configure basic project settings

---
+++ PitchDeck/PitchDeck/PitchDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchDeckApp.swift
+//  PitchDeck
+//
+//  Created by Anton Redkozubov on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PitchDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
